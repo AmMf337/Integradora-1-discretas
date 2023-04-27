3c36151d3525666d4770e05a9ec8a398ff5e2d61
SebasG42 <juanseguerrero14@gmail.com>
2023-04-26 23:29:54 -0500

    Added Remove and Swap methods to Heap

--- baseTools/Heap.java
+++ baseTools/Heap.java
@@ -43,6 +43,50 @@ public class Heap<K,T extends Comparable<T>>{
         size++;
 
     }
+    public insert(T node){
+        if(isFull()){
+            throw new IllegalStateException("Heap is full");
+        }
+        size++;
+
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public T remove() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Heap is empty.");
+        }
+        int removedValue = heap.get(0);
+        heap.set(1,heap.get(size));
+        size--;
+        bubbleDown();
+        return removedValue;
+    }
+    private void swap(int index1, int index2) {
+        T temp = heap.get(index1);
+        heap.set(index1,heap.get(index2));
+        heap.set(index2, temp);
+    }
+    
+
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int left(int index) {
+        return (index * 2) + 1;
+    }
+
+    private int right(int index) {
+        return (index * 2) + 2;
+    }
     
 
 } 
